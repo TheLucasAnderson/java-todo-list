ee86ded3ba7a33b47eb74a95de816a17102888b9
TheLucasAnderson <lucasand96@gmail.com>
2024-09-13 19:44:10 -0300

    Add email props on UserModel

--- src/main/java/br/com/thelucasanderson/todolist/user/UserModel.java
+++ src/main/java/br/com/thelucasanderson/todolist/user/UserModel.java
@@ -1,5 +1,6 @@
 package br.com.thelucasanderson.todolist.user;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -16,6 +17,9 @@ public class UserModel {
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
+
+    @Column(unique = true)
+    private String email;
 
     private String username;
     private String name;
