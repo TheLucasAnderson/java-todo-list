f21ec4f7b23761a576a89b3edf3f8439a5d8c812
TheLucasAnderson <lucasand96@gmail.com>
2024-09-13 21:02:05 -0300

    Encrypt password before create user

--- src/main/java/br/com/thelucasanderson/todolist/user/UserController.java
+++ src/main/java/br/com/thelucasanderson/todolist/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -23,6 +24,9 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email já cadastrado!");
         };
+
+        var encryptedPassword = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(encryptedPassword);
 
         UserModel userCreated;
         userCreated = this.userRepository.save(userModel);
