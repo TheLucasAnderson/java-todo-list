dc9c3c3ac1de359a1401c3e5c82e9f4ba09c1226
TheLucasAnderson <lucasand96@gmail.com>
2024-10-13 06:57:24 -0300

    Create filter to modify just the sended information on task

--- src/main/java/br/com/thelucasanderson/todolist/task/TaskController.java
+++ src/main/java/br/com/thelucasanderson/todolist/task/TaskController.java
@@ -1,5 +1,6 @@
 package br.com.thelucasanderson.todolist.task;
 
+import br.com.thelucasanderson.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -45,9 +46,10 @@ public class TaskController {
 
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
-        var idUser = request.getAttribute("idUser");
-        taskModel.setUserId((UUID) idUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
     }
 }
