4c5f84b54223c913b5a2a6ddc76d093d0497c68e
TheLucasAnderson <lucasand96@gmail.com>
2024-10-15 00:30:44 -0300

    Add conditionals to validate task owner and if exist

--- src/main/java/br/com/thelucasanderson/todolist/task/TaskController.java
+++ src/main/java/br/com/thelucasanderson/todolist/task/TaskController.java
@@ -45,11 +45,21 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
         var task = this.taskRepository.findById(id).orElse(null);
+        var idUser = request.getAttribute("idUser");
+
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body('Task não encontrada!');
+        }
+
+        if (!taskModel.getUserId().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body('Usuário não tem permissão nessa task');
+        }
 
         Utils.copyNonNullProperties(taskModel, task);
 
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.status(HttpStatus.OK).body(taskUpdated);
     }
 }
